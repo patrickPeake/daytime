079a471398276e7803f557689ca3763ebabc34d2
pjpDesktopWSL <pjp3@sfu.ca>
2024-01-25 14:40:49 -0800

    initial commit working on msg struct

---
+++ server.c
@@ -0,0 +1,56 @@
+#include <netinet/in.h>
+#include <time.h>
+#include <strings.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "msg.h"
+
+#define MAXLINE     4096    /* max text line length */
+#define LISTENQ     1024    /* 2nd argument to listen() */
+#define DAYTIME_PORT 3333
+
+int
+main(int argc, char **argv)
+{   
+    if (argc != 2) {
+        printf("usage: server <port>\n");
+        exit(1);
+    }
+    int     listenfd, connfd;
+    struct sockaddr_in servaddr;
+    char    buff[MAXLINE];
+    time_t ticks;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(atoi(argv[1])); /* daytime server */
+
+    bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    listen(listenfd, LISTENQ);
+
+    for ( ; ; ) {
+        connfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
+
+        char buffin[MAXLINE*4];
+        int ret = read(connfd, buffin, 4*MAXLINE);
+        if (ret > 0) {
+            buffin[ret] = '\0';  // Null-terminate the received data
+            printf("Received message from client: %s\n", buffin);
+        }
+
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        write(connfd, buff, strlen(buff));
+        printf("Sending response: %s", buff);
+
+        close(connfd);
+    }
+}
+
